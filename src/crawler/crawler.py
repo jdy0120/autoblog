16be09c42a2bb54b98ba216dc08b8fdde6d8d200
mango <jdy0120120@gmail.com>
2023-06-29 22:07:01 +0900

    update get blog content

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -17,6 +17,8 @@ def search_naver(query):
     # naver.com에 접근
     driver.get("https://map.naver.com/v5/search/" + query + '맛집')
     
+    
+    
     # searchIframe으로 이동
     driver.switch_to.frame("searchIframe")
     
@@ -37,37 +39,88 @@ def search_naver(query):
     
     driver.switch_to.frame('entryIframe')
     
+    try:
     #주소
-    driver.find_element(By.XPATH,f'')
-    
-    #주차장
-    driver.find_element(By.XPATH,f'')
-    
-    #시간
-    driver.find_element(By.XPATH,f'')
-    
-    #전화
-    driver.find_element(By.XPATH,f'')
-    
-    #가게
-    driver.find_element(By.XPATH,f'')
-    
-    #홈페이지
-    driver.find_element(By.XPATH,f'')
-    
-    #detail
-    driver.find_element(By.XPATH,f'')
-    
+      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[1]/div/a/span[1]')
+      print(element.text)
+    except:
+      print('주소가 없습니다.')
     
     try:
-      element = WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.XPATH, f"/html/body/div[3]/div/div/div/div[2]/div[2]/a[1]"))
-      )
-      
+      #주차장
+      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[2]/div/div/a/span[1]')
       element.click()
+      print(element.text)
+    except:
+      print('주차장 정보가 없습니다.')
+    
+    try:
+      #시간
+      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[3]/div/a')
+      element.click()
+      print(element.text)
+    except:
+      print('시간 정보가 없습니다.')
+    
+    try:
+      #전화
+      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[4]/div/span[1]')
+      element.click()
+      print(element.text)
+    except:
+      print('전화 정보가 없습니다.')
+    
+    try:
+      #가게
+      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[6]/div')
+      element.click()
+      print(element.text)
+    except:
+      print('가게 정보가 없습니다.')
+    
+    try:
+      #홈페이지
+      element = driver.find_elements(By.XPATH,f'')
+    except:
+      print('홈페이지 정보가 없습니다.')
+    
+    try:
+      #detail
+      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[8]/div/a/span[1]')
+      element.click()
+      print(element.text)
+    except:
+      print('detail 정보가 없습니다.')
+    
+    try:
+      element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,f'//*[@id="app-root"]/div/div/div/div[7]/div/div[8]/div[1]/ul')))
       
-    finally:
-      pass
+      list_items = element.find_elements(By.TAG_NAME, "li")
+      
+      for index,item in enumerate(list_items):
+        item.click()
+        
+        driver.switch_to.window(driver.window_handles[index+1])
+        
+            
+        
+        
+        driver.switch_to.frame('mainFrame')
+        blog_post = driver.find_element(By.XPATH,f'//*[@id="post-view223108686514"]/div/div/div[3]')
+        
+        print(blog_post.text)
+        
+    except:
+      print('블로그 리뷰 더보기 버튼이 없습니다.')
+    # try:
+    #   element = WebDriverWait(driver,10).until(
+    #     EC.presence_of_element_located((By.XPATH, f"/html/body/div[3]/div/div/div/div[2]/div[2]/a[1]"))
+    #   )
+      
+    #   element.click()
+      
+    # finally:
+    #   pass
 
     
     
