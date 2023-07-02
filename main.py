719b63b2a8a98691ee14a8d944ea7659d650b5f8
mango <jdy0120120@gmail.com>
2023-07-02 13:10:02 +0900

    update blog review button clisk

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from src.crawler.crawler import search_naver
-import csv
+
+import json
 
 if __name__ == "__main__":
     print('start')
     
-    dong_name_list = open('./data/korean_dong_name.csv','r')
-    reader = csv.reader(dong_name_list)
-    
-    for index,line in enumerate(reader):
-        if line[3] == '' or index == 0:
-            continue
+    dong_name_list = []
+    with open('./data/korean_dong_name.json', 'r', encoding='utf-8') as f:
+        dong_name_list = json.load(f)
+
+    for index,item in enumerate(dong_name_list):
         
-        search_word = line[1] + line[2] + line[3]
+        search_naver(item['address'])
         
-        search_naver(search_word)
         
--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -7,6 +7,75 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import ActionChains
 import time
 
+def search_list(driver):
+  try:
+    element = WebDriverWait(driver,10).until(
+      EC.presence_of_element_located((By.XPATH, f"/html/body/div[3]/div/div[3]/div[1]/ul/li[3]/div[1]/a[1]/div/div/span[1]"))
+    )
+    
+    element.click()
+  except:
+    return
+    
+  finally:
+    pass
+
+def search_detail(driver):
+  
+  try:
+  #주소
+    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[1]/div/a/span[1]')
+    print(element.text)
+  except:
+    print('주소가 없습니다.')
+  
+  try:
+    #주차장
+    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[2]/div/div/a/span[1]')
+    element.click()
+    print(element.text)
+  except:
+    print('주차장 정보가 없습니다.')
+  
+  try:
+    #시간
+    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[3]/div/a')
+    element.click()
+    print(element.text)
+  except:
+    print('시간 정보가 없습니다.')
+  
+  try:
+    #전화
+    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[4]/div/span[1]')
+    element.click()
+    print(element.text)
+  except:
+    print('전화 정보가 없습니다.')
+  
+  try:
+    #가게
+    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[6]/div')
+    element.click()
+    print(element.text)
+  except:
+    print('가게 정보가 없습니다.')
+  
+  try:
+    #홈페이지
+    element = driver.find_elements(By.XPATH,f'')
+  except:
+    print('홈페이지 정보가 없습니다.')
+  
+  try:
+    #detail
+    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[8]/div/a/span[1]')
+    element.click()
+    print(element.text)
+  except:
+    print('detail 정보가 없습니다.')
+    
+
 def search_naver(query):
     # 크롬 웹드라이버의 경로를 입력하세요
     
@@ -14,25 +83,18 @@ def search_naver(query):
     chrome_options.add_experimental_option("detach", True)
     
     driver = webdriver.Chrome()
-    action = ActionChains(driver)
+
+    print(query)
 
     # naver.com에 접근
     driver.get("https://map.naver.com/v5/search/" + query + '맛집')
     
     
-    
     # searchIframe으로 이동
     driver.switch_to.frame("searchIframe")
     
-    try:
-      element = WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.XPATH, f"/html/body/div[3]/div/div[3]/div[1]/ul/li[3]/div[1]/a[1]/div/div/span[1]"))
-      )
-      
-      element.click()
-      
-    finally:
-      pass
+    # 음식점 리스트 클릭
+    search_list(driver)
 
     time.sleep(3)
     
@@ -41,64 +103,14 @@ def search_naver(query):
     
     driver.switch_to.frame('entryIframe')
     
-    try:
-    #주소
-      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[1]/div/a/span[1]')
-      print(element.text)
-    except:
-      print('주소가 없습니다.')
+    search_detail(driver)
     
     try:
-      #주차장
-      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[2]/div/div/a/span[1]')
-      element.click()
-      print(element.text)
-    except:
-      print('주차장 정보가 없습니다.')
-    
-    try:
-      #시간
-      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[3]/div/a')
-      element.click()
-      print(element.text)
-    except:
-      print('시간 정보가 없습니다.')
-    
-    try:
-      #전화
-      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[4]/div/span[1]')
-      element.click()
-      print(element.text)
-    except:
-      print('전화 정보가 없습니다.')
-    
-    try:
-      #가게
-      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[6]/div')
-      element.click()
-      print(element.text)
-    except:
-      print('가게 정보가 없습니다.')
-    
-    try:
-      #홈페이지
-      element = driver.find_elements(By.XPATH,f'')
-    except:
-      print('홈페이지 정보가 없습니다.')
-    
-    try:
-      #detail
-      element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[8]/div/a/span[1]')
-      element.click()
-      print(element.text)
-    except:
-      print('detail 정보가 없습니다.')
-    
-    try:
-      time.sleep(3)
       driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
       
-      element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,f'//*[@id="app-root"]/div/div/div/div[7]/div/div[8]/div[2]/a')))
+      time.sleep(3)
+      
+      element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,f"//a[contains(text(), '블로그리뷰 더보기')]")))
       element.click()
     except:
       print('블로그 리뷰 더보기 버튼이 없습니다.')
@@ -130,21 +142,5 @@ def search_naver(query):
     except:
       print('블로그 리뷰가 없습니다.')
     
-      
-      
-      
 
-    # try:
-    #   element = WebDriverWait(driver,10).until(
-    #     EC.presence_of_element_located((By.XPATH, f"/html/body/div[3]/div/div/div/div[2]/div[2]/a[1]"))
-    #   )
-      
-    #   element.click()
-      
-    # finally:
-    #   pass
-
-    
-    
-    while(True):
-      pass
+    driver.close()
