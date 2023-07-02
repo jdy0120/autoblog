3da34daa1a93752fa2e945b401c221aead9194cc
mango <jdy0120120@gmail.com>
2023-07-02 19:01:49 +0900

    update crawler complete

--- src/chatgpt/answer_api_chat_gpt.py
+++ src/chatgpt/answer_api_chat_gpt.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
+# openai.api_base = os.getenv("OPENAI_API_BASE")
 openai.api_key = os.getenv("CHAT_GPT_API")
 
 def answer_api_chat_gpt(text):
   response = openai.Completion.create(
-    model='gpt-3.5-turbo',
-    prompt='chat gpt 사용 방법에 대해 알려줘'
+    model="text-davinci-003", # The deployment name you chose when you deployed the ChatGPT or GPT-4 model.
+    prompt=f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"\n\nContext: doyeon\n\n---\n\nQuestion: can you answer korean?\nAnswer:"
   )
   
-  print(response)
+  answer =response["choices"][0]["text"].strip()
+  print(answer)
--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -8,14 +8,16 @@ from selenium.webdriver import ActionChains
 import time
 
 def list_element(driver):
-  element = driver.find_elements(By.XPATH,f'/html/body/div[3]/div/div[3]/div[1]/ul/li')
-  for e in element:
-    print(e.text)
-
-def search_list(driver):
   try:
-    element = WebDriverWait(driver,10).until(
-      EC.presence_of_element_located((By.XPATH, f"/html/body/div[3]/div/div[3]/div[1]/ul/li[3]/div[1]/a[1]/div/div/span[1]"))
+    element = driver.find_element(By.XPATH,f'//*[@id="_pcmap_list_scroll_container"]/ul')
+    return element
+  except:
+    return False
+
+def search_list(shop):
+  try:
+    element = WebDriverWait(shop,10).until(
+      EC.presence_of_element_located((By.XPATH, f"./div[1]/a[1]/div/div/span[1]"))
     )
     
     element.click()
@@ -26,68 +28,50 @@ def search_list(driver):
     pass
 
 def search_detail(driver):
+  shop_info = dict()
   
-  element = driver.find_element(By.CLASS_NAME,f'place_section_content')
-  all_down_arrow = element.find_elements(By.XPATH, f"//a[@aria-expanded='false']")
+  element = driver.find_element(By.CLASS_NAME,f'PIbes')
+  all_infos = element.find_elements(By.XPATH, './div')
   
-  for e in all_down_arrow:
-    e.click()
-  
-  time.sleep(10)
-  try:
-  #주소
-    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[1]/div/a/span[1]')
-    print(element.text)
-  except:
-    print('주소가 없습니다.')
-  
-  try:
-    #주차장
-    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[2]/div/div/a/span[1]')
-    element.click()
-    print(element.text)
-  except:
-    print('주차장 정보가 없습니다.')
-  
-  try:
-    #시간
-    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[3]/div/a')
-    element.click()
-    print(element.text)
-  except:
-    print('시간 정보가 없습니다.')
-  
-  try:
-    #전화
-    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[4]/div/span[1]')
-    element.click()
-    print(element.text)
-  except:
-    print('전화 정보가 없습니다.')
-  
-  try:
-    #가게
-    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[6]/div')
-    element.click()
-    print(element.text)
-  except:
-    print('가게 정보가 없습니다.')
-  
-  try:
-    #홈페이지
-    element = driver.find_elements(By.XPATH,f'')
-  except:
-    print('홈페이지 정보가 없습니다.')
-  
-  try:
-    #detail
-    element = driver.find_element(By.XPATH,f'//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[8]/div/a/span[1]')
-    element.click()
-    print(element.text)
-  except:
-    print('detail 정보가 없습니다.')
-    
+  for e in all_infos:
+    try:
+      # aria-expanded="false" 속성을 가진 a 태그를 찾습니다.
+      a_tag = e.find_element(By.XPATH,f".//a[@aria-expanded='false']")
+      
+      # 찾은 a 태그를 클릭합니다.
+      a_tag.click()
+      
+    except:
+        # a 태그가 없으면 무시하고 진행합니다.
+        pass
 
+
+    time.sleep(0.1)
+    print('\n'.split(e.text)[0])
+    
+    try:
+      info_key = e.find_element(By.XPATH,'./strong')
+      print(info_key.text)
+      
+      info_value = e.find_element(By.XPATH,'./div')
+      print(info_value.text)
+      
+      if (info_key.text != '정보 수정 제안'):
+        shop_info[info_key.text] = info_value.text
+
+    except:
+      pass
+    
+    try:
+      a_tag = e.find_element(By.XPATH,f".//a[@aria-expanded='true']")
+      
+      a_tag.click()
+      
+    except:
+        pass
+
+  print(shop_info)
+  
 def search_naver(query):
     # 크롬 웹드라이버의 경로를 입력하세요
     
@@ -103,56 +87,65 @@ def search_naver(query):
     # searchIframe으로 이동
     driver.switch_to.frame("searchIframe")
     
-    list_element(driver)
-    
-    # 음식점 리스트 클릭
-    search_list(driver)
-
     time.sleep(3)
     
-    # 부모 프레임으로 이동 후 음식점 상세 프레임으로 이동
-    driver.switch_to.parent_frame()
+    element = list_element(driver)
+    if (element == False):
+      driver.close()
+      return
     
-    driver.switch_to.frame('entryIframe')
+    shop_list = element.find_elements(By.XPATH, "./li")
     
-    search_detail(driver)
-    
-    try:
-      driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-      
+    for shop in shop_list:
+      # 음식점 리스트 클릭
+      search_list(shop)
+
       time.sleep(3)
       
-      element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,f"//a[contains(., '더보기') and .//span[contains(., '블로그리뷰')]]")))
-      element.click()
-    except:
-      print('블로그 리뷰 더보기 버튼이 없습니다.')
+      # 부모 프레임으로 이동 후 음식점 상세 프레임으로 이동
+      driver.switch_to.parent_frame()
+      
+      driver.switch_to.frame('entryIframe')
+      
+      search_detail(driver)
+      
+      try:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        
+        time.sleep(3)
+        
+        element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,f"//a[contains(., '더보기') and .//span[contains(., '블로그리뷰')]]")))
+        element.click()
+      except:
+        print('블로그 리뷰 더보기 버튼이 없습니다.')
 
-    try:
-      element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,f'//*[@id="app-root"]/div/div/div/div[7]/div[3]/div/div[1]/ul')))
-      list_items = element.find_elements(By.TAG_NAME, "li")
-    
-      for index,item in enumerate(list_items):
-        try:
-          driver.switch_to.frame('entryIframe')
-        except:
-          print('entryIframe이 없습니다.')
-        item.click()
+      try:
+        element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,f'//*[@id="app-root"]/div/div/div/div[7]/div[3]/div/div[1]/ul')))
+        list_items = element.find_elements(By.TAG_NAME, "li")
       
-        driver.switch_to.window(driver.window_handles[index+1])
+        for index,item in enumerate(list_items):
+          try:
+            driver.switch_to.frame('entryIframe')
+          except:
+            print('entryIframe이 없습니다.')
+          item.click()
         
-        driver.switch_to.frame('mainFrame')
+          driver.switch_to.window(driver.window_handles[index+1])
+          
+          driver.switch_to.frame('mainFrame')
+          
+          blog_post = driver.find_elements(By.CSS_SELECTOR,f"div[id*='post-view']")
+          
+          for post in blog_post:
+            print(post.text)
         
-        blog_post = driver.find_elements(By.CSS_SELECTOR,f"div[id*='post-view']")
+          driver.switch_to.window(driver.window_handles[0])
         
-        for post in blog_post:
-          print(post.text)
+      except:
+        print('블로그 리뷰가 없습니다.')
+        
+      driver.switch_to.parent_frame()
+      driver.switch_to.frame("searchIframe")
       
-        # print(blog_post.text)
-        
-        driver.switch_to.window(driver.window_handles[0])
-      
-    except:
-      print('블로그 리뷰가 없습니다.')
-    
 
     driver.close()
